4be986484df272bdfe72445f7943a0c90f6d1b3b
Peter Velkov <kidroca@gmail.com>
2016-12-28 21:30:50 +0200

    feat(movie-list.component): created new component
    Created the movies list component and added the MoviesData service

---
+++ app/movies/movies-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Movie} from '../data/models/movie';
+import {MovieDataService} from '../data/movie-data.service';
+
+@Component({
+    selector: 'mvdb-movies-list',
+    templateUrl: 'movies-list.component.html',
+    providers: [MovieDataService]
+})
+export class MoviesListComponent implements OnInit {
+
+    movies: Movie[] = [];
+    pageTitle: string = 'Movies List';
+
+    constructor(private movieData: MovieDataService) { }
+
+    private initMovies(): void {
+        this.movieData
+            .getMovies()
+            .then(movies => this.movies = movies);
+    }
+
+    ngOnInit(): void {
+        this.initMovies();
+    }
+}
