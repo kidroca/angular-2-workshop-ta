ea7760419257a814810e023457b08cdd2375505d
Peter Velkov <kidroca@gmail.com>
2016-12-29 18:41:09 +0200

    feat(order-by.pipe): create new pipe

--- app/pipes/order-by.pipe.ts
+++ app/pipes/order-by.pipe.ts
@@ -1,3 +1,24 @@
-/**
- * Created by kidroca on 29.12.2016 г..
- */
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'orderBy'
+})
+export class OrderByPipe<T extends Object> implements PipeTransform {
+
+    transform(collection: T[], key = '', inReverse = false): T[] {
+
+        if (collection.length <= 1) {
+            return collection;
+        }
+
+        return collection.sort((a, b) => {
+
+            a = a[key] || a;
+            b = b[key] || b;
+
+            let result = +(a > b) || +(a === b) - 1;
+
+            return inReverse ? result * -1 : result;
+        });
+    }
+}
