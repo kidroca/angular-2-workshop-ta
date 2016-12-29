ab98c5a7fb526490e462945d61b0319118bf1220
Peter Velkov <kidroca@gmail.com>
2016-12-29 09:30:28 +0200

    refactor(app.module): registered new component to declarations

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-
 import {Ng2BootstrapModule} from 'ng2-bootstrap/ng2-bootstrap';
 
+import { AppComponent } from './app.component';
+import {MoviesListComponent} from './movies/movies-list.component';
+
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent, MoviesListComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
