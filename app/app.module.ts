e7165eeaf84498443f640167f4abd97f1249c04d
Peter Velkov <kidroca@gmail.com>
2016-12-29 10:14:29 +0200

    chore(app.module): imported additional modules
    Added `ng-bootstrap` & `http` to @NgModule imports

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {Ng2BootstrapModule} from 'ng2-bootstrap/ng2-bootstrap';
+import {Ng2BootstrapModule} from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import {MoviesListComponent} from './movies/movies-list.component';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, Ng2BootstrapModule, HttpModule],
     declarations: [AppComponent, MoviesListComponent],
     bootstrap: [AppComponent]
 })
