e78fd25ed6453b5d578f583b5ed80b0c9cbacb51
Peter Velkov <kidroca@gmail.com>
2016-12-30 11:14:12 +0200

    refactor(app.module): extracted routing to app-routing.module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,29 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared.module';
 import {CoreModule} from './core/core.module';
-import {MoviesListComponent} from './movies/list/movies-list.component';
-import {MovieDetailComponent} from './movies/detail/movie-detail.component';
+import {AppRoutingModule} from '../app-routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
-        RouterModule.forRoot([{
-            path: '',
-            redirectTo: '/movies',
-            pathMatch: 'full'
-        }, {
-            path: 'movies',
-            component: MoviesListComponent
-        }, {
-            path: 'movies/:id',
-            component: MovieDetailComponent
-        }]),
         CoreModule,
-        SharedModule
+        SharedModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent
